8a0dd8ac1106982998189f9fa4a078cbb2b163be
marcoosavelino <71901248@aluno.faculdadecotemig.br>
2022-09-24 10:46:38 -0300

    Commit Final

--- Fundamentos JavaScript/desafio4.js
+++ Fundamentos JavaScript/desafio4.js
@@ -1,4 +1,4 @@
-// Construa uma função que receba dois valores de entrada do usuário e faça da soma desses valores.
+// Construa uma função que receba dois valores de entrada do usuário e faça a soma desses valores.
 
 function soma(a, b){
     return parseInt(a) + parseInt(b);
